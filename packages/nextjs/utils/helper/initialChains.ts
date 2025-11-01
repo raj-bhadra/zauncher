69d5282bcee4fdc8586b62cd77d8caa6f1750f19
r <50800165+raj-bhadra@users.noreply.github.com>
2025-11-01 15:37:29 +0530

    Update initial chains

--- packages/nextjs/utils/helper/initialChains.ts
+++ packages/nextjs/utils/helper/initialChains.ts
@@ -1,5 +1,14 @@
-const initialMockChains = {
-    31137: "http://localhost:8545",
+import { RPC_CHAIN_NAMES, getAlchemyHttpUrl } from ".";
+import * as chains from "viem/chains";
+
+let initialMockChains: Record<number, string> = {
+  31137: "http://localhost:8545",
 };
+
+if (process.env.NODE_ENV === "production") {
+  initialMockChains = {
+    [chains.sepolia.id]: `https://eth-mainnet.g.alchemy.com/v2/${process.env.NEXT_PUBLIC_ALCHEMY_API_KEY}`,
+  };
+}
 
 export default initialMockChains;
